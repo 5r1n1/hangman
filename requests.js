fd8062e56f703cc1bb20670d8d3db3fc6845448b
5r1n1 <srinig@msn.com>
2018-08-01 22:49:08 +0530

    Fetch instead of XMLHTTPRequest

--- requests.js
+++ requests.js
@@ -1,14 +1,5 @@
-const generatePuzzle = (wordCount = 0) => new Promise ((resolve, reject) => {
-    const req = new XMLHttpRequest()
-    req.addEventListener ('readystatechange', e => {
-        if (e.target.readyState === 4 && e.target.status === 200)
-            resolve (JSON.parse(e.target.responseText).puzzle)
-        else if (e.target.readyState === 4)
-            reject (`Error: HTTP Status ${e.target.status}`)
-    })
-
-    wordCount && wordCount < 10 ? 
-        req.open('GET', `https://puzzle.mead.io/puzzle?wordCount=${wordCount}`) :
-        req.open('GET', 'https://puzzle.mead.io/puzzle/')
-    req.send()
-})
+const generatePuzzle = (wordCount = 2) =>
+    fetch(`https://puzzle.mead.io/puzzle?wordCount=${wordCount}`).then (resp => {
+        if (resp.status === 200) return resp.json().puzzle
+        else throw new Error ('Unable to fetch puzzle') 
+    }).catch (error => console.log (error))
