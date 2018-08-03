8f3bbc1ae9f68ecee5f161c0e7be78605eeb063e
5r1n1 <37212404+5r1n1@users.noreply.github.com>
2018-08-03 15:37:44 +0530

    Update requests.js

--- requests.js
+++ requests.js
@@ -1,5 +1,4 @@
-const generatePuzzle = (wordCount = 2) => 
-    fetch (`https://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
+const generatePuzzle = (wordCount = 2) => fetch (`https://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
         .then (resp => resp.status === 200 ? resp.json() : Error (`Unable to fetch puzzle. HTTP Status - ${resp.status}`))
         .then (data => data.puzzle)
         .catch (error => console.log (error))
