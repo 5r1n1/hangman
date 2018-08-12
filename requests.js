66d31c870c81d5c3959aa65a4244e69dffe398af
5r1n1 <srinig@msn.com>
2018-08-12 17:48:36 +0530

    Added basic Stylesheet

--- requests.js
+++
@@ -1,4 +0,0 @@
-const generatePuzzle = (wordCount = 2) => fetch (`https://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
-    .then (resp => resp.status === 200 ? resp.json() : Error (`Unable to fetch puzzle. HTTP Status - ${resp.status}`))
-    .then (data => data.puzzle)
-    .catch (error => console.log (error))
