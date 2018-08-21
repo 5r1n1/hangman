e105270dbe9d89991f96e5ed892d92aaedfc77b2
Srinivas Gopalakrishnan <srini@5cd4214.rbd741>
2018-08-21 17:50:09 +0530

    Added Sounds and made Keyboard Friendly

--- scripts/app.js
+++ scripts/app.js
@@ -11,7 +11,7 @@ const renderGame = game => {
     document.querySelector('#guessed').innerHTML = game.guessedLetters
     document.querySelector('#remaining').textContent = game.lettersRemaining.toString()
     document.querySelector('#tries-left').textContent = `${game.remainingAttempts}/${game.maxAttempts}`
-    if (game.status === 'won' || game.status === 'lost' ) {    
+    if (game.status === 'won' || game.status === 'lost' ) {
         document.querySelector('#display-word').textContent = game.originalWord
         document.querySelector('#display-word').setAttribute('style',
             (game.status === 'won') ? 'color:chartreuse' : 'color:orangered')
@@ -29,17 +29,29 @@ document.querySelector('#guess-form').addEventListener('submit', e => {
     renderGame (game1)
 })
 
+document.querySelector('#guess').addEventListener('input', (e) => {
+    const data = e.target.value.toLowerCase()
+    if (data && ((data < 'a') || (data > 'z'))) {
+        document.querySelector('#guess-msg').textContent = 'Please enter a letter'
+        e.target.value = ''
+        playError()
+    } 
+})
+
 document.querySelector('#word-count').addEventListener('input', (e) => {
     const data = e.target.value
     if (data && ((data < '1') || (data > '9'))) {
         document.querySelector('#new-game-msg').textContent = 'Please enter word count'
         e.target.value = ''
+        playError()
     } else document.querySelector('#new-game-msg').textContent = ''
 })
 
 document.querySelector('#new-game-form').addEventListener('submit', e => {
     e.preventDefault()
     generatePuzzle (e.target.elements.wordCount.value).then (
-        puzzle => renderGame (game1 = new Hangman (puzzle, maxTries)),
-        err => {throw err})
+        puzzle => {
+            renderGame (game1 = new Hangman (puzzle, maxTries))
+            document.getElementById('guess').focus()
+        },err => {throw err})
 })
